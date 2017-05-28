14a1393bc6a2d21ef98056d00e7a4c20d3927f12
citronne <tigresse416@gmail.com>
2017-05-28 23:06:53 +0200

    add font awesome add html/css maquettes

--- server/scripts/scan.php
+++ server/scripts/scan.php
@@ -1,46 +1,79 @@
 <?php
     class Manga {
         private $name;
-        private $volume;
-        private $path;
+        private $volumes;
+
+        public function __construct($name) {
+            $this->name = $name;
+            $this->volumes = array();
+        }
 
         public function getName() {
             return $this->name;
         }
 
-        public function getVolume() {
-            return $this->volume;
+        public function getVolumes() {
+            return $this->volumes;
+        }
+
+        public function addVolume($volume) {
+            array_push($this->volumes, $volume);
+        }
+
+        public function __toString() {
+            return $this->name;
+        }
+    }
+
+    class Volume {
+        private $volume_number;
+        private $path;
+        private $add_date;
+        private $access_date;
+        private $read;
+        private $page_number;
+        private $manga;
+
+        public function __construct($volume_number, $path, $manga) {
+            $this->volume_number = $volume_number;
+            $this->path = $path;
+            $this->manga = $manga;
         }
 
         public function getPath() {
             return $this->path;
         }
-
-        public function __construct($name,$volume,$path) {
-            $this->name = $name;
-            $this->volume = $volume;
-            $this->path = $path;
-        }
     }
 
-
     function scan($library_path) {
-        $files = scandir($library_path);
-        $mangas = array();
+        $files = scandir($library_path); //scanner des fichiers et envoie un array
+        $mangas = array(); //creer un array pour ranger des mangas par nom
 
         for($i = 0; $i < count($files); $i++) {
             $manga_file = $files[$i];
             if($manga_file != "." && $manga_file != "..") {
                 echo $manga_file, PHP_EOL;
-                $a = explode(".", $manga_file);
-                echo $a[0], PHP_EOL;
+                $a = explode(".", $manga_file); //separer avec "."
+                echo $a[0], PHP_EOL; //"nom de manga" _ "numero de volume"
 
-                $b = explode("_", $a[0]);
-                echo $b[0], PHP_EOL;
-                echo $b[1], PHP_EOL;
+                $b = explode("_", $a[0]); //separer avec "_"
+                echo $b[0], PHP_EOL; //nom
+                echo $b[1], PHP_EOL; //numero de volume
                 echo $library_path . $manga_file, PHP_EOL;
-                $manga = new Manga($b[0], $b[1], $library_path . "\\" . $manga_file);
-                array_push($mangas, $manga);
+
+                $manga_name = $b[0];
+                if(!in_array($manga_name, $mangas)){
+                    $manga = new Manga($manga_name); //creer le nouveau
+                    $mangas[$manga_name] = $manga; //et mettre
+                } else {
+                    $manga = $mangas[$manga_name];
+                }
+
+                $volume = new Volume($b[1], $library_path . "\\" . $manga_file, $manga);
+                $manga->addVolume($volume);
+                //$manga = new Manga($b[0], $b[1], $library_path . "\\" . $manga_file);
+                //array_push($mangas, $manga);
+
             }
         }
 
@@ -48,7 +81,11 @@
     }
 
     $mangas = scan("D:\\manga");
+    $mangamamaladeboy = $mangas["mamaladeboy"];
 
-    echo $mangas[0]->getName(), PHP_EOL;
+    echo count($mangamamaladeboy->getVolumes());
+    $volumes = $mangamamaladeboy->getVolumes();
+    echo $volumes[0]->getPath();
+
 
 ?>
