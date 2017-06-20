4ca925e21e1d0d941fa0de03dcfa348036869500
citronne <tigresse416@gmail.com>
2017-06-20 15:37:46 +0200

    add new version of reader.twig

--- public/scripts/reader.js
+++ public/scripts/reader.js
@@ -2,10 +2,13 @@
  * Created by sayaka on 2017/06/18.
  */
 function getData() {
-   $.getJSON(window.location.pathname + "/reader", function (data) {
-        //console.log(data);
+   $.getJSON(window.location.pathname + "/reader", function (volume_data) {
+        //console.log(data)
+       console.log(volume_data.volumeNumber);
    });
 }
+
+$( ".current-page" ).attr( "src", volume.volumeNumber);
 
 $('.fa fa-arrow-circle-left').click(function () {
     pagePlusOne();
