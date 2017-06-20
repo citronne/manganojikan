97a2353980ae987c46013e8bf708c4054da8ef43
citronne <tigresse416@gmail.com>
2017-06-20 22:53:32 +0200

    modify page button for reader

--- public/scripts/reader.js
+++ public/scripts/reader.js
@@ -1,25 +1,65 @@
 /**
  * Created by sayaka on 2017/06/18.
  */
+var data;
+var i = 0;
+
 function getData() {
    $.getJSON(window.location.pathname + "/reader", function (volume_data) {
-        //console.log(data)
-       console.log(volume_data.volumeNumber);
+       console.log(volume_data.cover);
+       data = volume_data;
+       $( ".current-page>img" ).attr( "src", volume_data.cover);
    });
 }
 
-$( ".current-page" ).attr( "src", volume.volumeNumber);
-
-$('.fa fa-arrow-circle-left').click(function () {
-    pagePlusOne();
+$('.fa.fa-arrow-circle-left').click(function () {
+    changePage(false);
 });
 
-$('.fa fa-arrow-circle-right').click(function () {
-    pageMinusOne();
+$('.fa.fa-arrow-circle-right').click(function () {
+    changePage(true);
 });
 
-function pagePlusOne() {
+function toggleFullScreen(elem) {
+    // ## The below if statement seems to work better ## if ((document.fullScreenElement && document.fullScreenElement !== null) || (document.msfullscreenElement && document.msfullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+        if (elem.requestFullScreen) {
+            elem.requestFullScreen();
+        } else if (elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullScreen) {
+            elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+        }
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
 
+function changePage(inc) {
+    var path = window.location.pathname;
+    var files = data.file_names;
+    var file;
+    if (inc && i < files.length - 1) {
+        file = files[++i];
+    } else if (!inc && i > 0) {
+        file = files[--i];
+    }
+    if (file) {
+        $( ".current-page>img" ).attr( "src", path + "/" + file);
+    }
 }
 
 getData();
+$(".fullscreenBtn").click(function() {
+    toggleFullScreen($(".container")[0]);
+});
